add0b52f3f9298d1f249d6bc359399a0d14c6efe
Ori Marash <ori@marash.net>
2020-08-08 23:47:59 +0100

    Get title and description from webpage
    (...rather than Google)
    
    Also moved all the code to the model raather than the view.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,6 +3,69 @@ require 'rss'
 
 class Feed < ApplicationRecord
   def entries
-    Nokogiri::XML(open(self.google_alerts_url)).css("entry")
+    document = Nokogiri::XML(open(self.google_alerts_url))
+
+    document.css("entry").map do |entry|
+      # Get the page of the entry
+      google_provided_link = entry.at_css("link").attributes["href"].value
+      decoded_link = URI::decode_www_form(google_provided_link)
+      parameters = decoded_link.to_h
+      page_link = URI(parameters["url"])
+      page = Net::HTTP.get(page_link)
+      document = Nokogiri::HTML.parse(page)
+
+      if entry.at_css("published").content
+        pubDate = DateTime.parse(entry.at_css("published").content).rfc2822
+      end
+
+      # We now have the parsed HTML of the webpage in the var "document"
+      # which we can use to get all the info we need
+
+      # Title
+      # =====
+      # 1) <meta property="og:title">
+      # 2) <title>
+      # 3) first h1
+      # 4) (no title)
+
+      title = "(no title)"
+
+      if document.at_css("meta[property='og:title']")
+        title = document.at_css("meta[property='og:title']").attribute("content").content
+      elsif document.at_css("title")
+        title = document.at_css("title").content
+      elsif document.at_css("h1")
+        title = document.at_css("h1").content
+      end
+
+      # Description
+      # ===========
+      # 1) <meta property="og:description" content="">
+      # 2) <meta name="description">
+      # 3) The first <p> tag in the <article>
+      # 4) The first <p> tag
+      # 5) (no description)
+
+      description = "(no description)"
+
+      if document.at_css("meta[property='og:descriotion']")
+        description = document.at_css("meta[property='og:descriotion']")["content"]
+      elsif document.at_css("meta[name='description']")
+        description = document.at_css("meta[name='description']")["content"]
+      elsif document.at_css("article p")
+        description = document.at_css("article p").content
+      elsif document.at_css("p")
+        description = document.at_css("p").content
+      end
+
+
+      # Return a hash of all the values
+      {
+        :title => title,
+        :description => description,
+        :link => page_link,
+        :pub_date => pubDate
+      }
+    end
   end
 end
--- app/views/feeds/show.rss.builder
+++ app/views/feeds/show.rss.builder
@@ -1,4 +1,6 @@
 require 'date'
+require 'cgi'
+require 'net/http'
 
 xml.instruct! :xml, :version => "1.0"
 xml.rss("version" => "2.0", "xmlns:dc" => "http://purl.org/dc/elements/1.1/") do
@@ -13,22 +15,12 @@ xml.rss("version" => "2.0", "xmlns:dc" => "http://purl.org/dc/elements/1.1/") do
 
     @feed.entries.each do |entry|
       xml.item do
-        # Parse the title
-        title = entry.css("title").text
-        xml.title Nokogiri::HTML.parse(title).text
-        
-        # Parse the description
-        content = entry.css("content").text
-        xml.description Nokogiri::HTML.parse(content).text
-
-        # Date
-        if entry.at_css("published").content
-          xml.pubDate DateTime.parse(entry.at_css("published").content).rfc2822
-        end
-
-        # Guid
-        xml.guid entry.at_css("link").attributes["href"]
-        xml.link entry.at_css("link").attributes["href"]
+        puts entry
+        xml.title entry[:title]
+        xml.description entry[:description]
+        xml.guid entry[:link]
+        xml.link entry[:link]
+        xml.pubDate entry[:pub_date]
       end
     end
   end
